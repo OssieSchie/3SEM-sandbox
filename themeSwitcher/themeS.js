c195ff898b04b5430e218209a1c70900cfbfaed9
Ossie <osvald.rye@gmail.com>
2024-02-12 11:34:04 +0100

    2nd 2day

--- themeSwitcher/themeS.js
+++ themeSwitcher/themeS.js
@@ -1,1 +1,4 @@
 //tger value input fra drop down og smider dem ind i data-theme i html, kig i languageSwitcher, kald dataset.theme = evt.target.value
+
+//document.querySelector("#language").addEventListener("change", (event) => {
+
