e677dce4e1cd50d1fb95320ed7455431fadfaa4f
Ossie <osvald.rye@gmail.com>
2024-02-19 09:38:55 +0100

    toDo liste dokumenter med noget pseudo kode

---
+++ toDoListe/toDo.js
@@ -0,0 +1,16 @@
+const taskArray = [];
+
+function makeNewTask(){
+    const task = {name: "joe", done: false};
+    taskArray.push(task);
+}
+
+
+
+//
+
+//Tilføj item til liste //generer id til liste //check om ID'et er taget (generer måske ID'et baseret på hvor mange tasks du har lavet)
+
+//fjerne item fra listen //føj til "trash array" (array med max 5 objecter, objecter kan restoreres)
+
+//Tegn hvert item i arrayet (se i animal base filtering) //clear alle elementer og tegn dem igen når funktionen bliver kaldt
