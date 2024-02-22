80801aee2ee672e4958a57f5f4f024e89ddf55d6
OssieSchie <osvald.rye@gmail.com>
2024-02-22 15:49:43 +0100

    working version - missing polish

--- toDoListe/toDo.js
+++ toDoListe/toDo.js
@@ -1,4 +1,5 @@
 const taskArray = [];
+const finishedTaskArray = [];
 
 /* window.addEventListener("load", pushLocal);
 
@@ -57,7 +58,7 @@ function makeNewTask(){
 
 function prepareList(){
     document.querySelector("#tasks").innerHTML = "";
-
+    
     const localArray = localStorage.getItem("Task Array");
     
     const parsedArray = JSON.parse(localArray);
@@ -73,17 +74,109 @@ function displayTask(showTask){
     
     clone.querySelector("[data-field=taskString]").textContent = showTask.name;
     clone.querySelector("[data-field=taskAmount]").textContent = showTask.amount;
+    clone.querySelector("[data-id=defaultID]").setAttribute("data-id", showTask.id);
     
     document.querySelector("#tasks").appendChild(clone);
+    
+    document.querySelectorAll("[data-type=button]").forEach(button=>{
+        button.addEventListener("click", btnFunction)
+    })
+    
+    //btnEventListener();
 }
 
+/* function btnEventListener(){
+    //const cloneBtn = document.querySelector("#mCol #tasks #toDoBtns button");
+    //const cloneBtn = document.querySelectorAll("[data-type=button]");
+    document.querySelectorAll("[data-type=button]").addEventListener("click", btnFunction);
+    //const cloneFinBtn = document.querySelectorAll("[data-field=finBtn]");
+    //const cloneDelBtn = document.querySelectorAll("[data-field=DelBtn]");
+    //cloneBtn.addEventListener("click", btnFunction);
+    //cloneFinBtn.addEventListener("click", btnFunction);
+    //cloneDelBtn.addEventListener("click", btnFunction);
+    
+} */
 
+function btnFunction(evt){
+    const parentID = evt.currentTarget.parentNode.parentNode.getAttribute("data-id");
+    
+    if(evt.currentTarget.dataset.field === "finBtn"){
+        console.log("finished task, ", "id: ", parentID);
 
+        const id = taskArray.findIndex(task => task.id === parentID);
 
-//
+        const removedId = taskArray.splice(id, 1)[0];
 
-//DONE ----- Tilføj item til liste //generer id til liste //check om ID'et er taget (generer måske ID'et baseret på hvor mange tasks du har lavet)
+        finishedTaskArray.push(removedId);
+        taskArray.push();
+        
+        localStorage.setItem("Finished Task Array", JSON.stringify(finishedTaskArray));
+        localStorage.setItem("Task Array", JSON.stringify(taskArray));
+        
+        //console.table(taskArray);
+        //console.table(finishedTaskArray);
+        
+        prepareFinishedList();
+        prepareList();
+        
+        //get the currentTarget ID from the data-id attribute, slice the selected object out, push it to the completed array, push new taskArray - the sliced bit
+    }else if(evt.currentTarget.dataset.field === "delBtn"){
+        console.log("deleted task, ", "id: ", parentID);
+        
+        const id = taskArray.findIndex(task => task.id === parentID);
+        
+        const removedId = taskArray.splice(id, 1)[0];
+        
+        taskArray.push();
+        localStorage.setItem("Task Array", JSON.stringify(taskArray));
+        
+        prepareList();
+    }
+}
 
-//fjerne item fra listen //føj til "trash array" (array med max 5 objecter, objecter kan restoreres)
+function prepareFinishedList(){
+    document.querySelector("#finishedTasks").innerHTML = "";
+    
+    const localArray = localStorage.getItem("Finished Task Array");
+    
+    const parsedArray = JSON.parse(localArray);
+    //const parseLocalArray = JSON.parse(localArray);
+    
+    parsedArray.forEach(displayFinishedTask);
+}
 
-//Tegn hvert item i arrayet (se i animal base filtering) //clear alle elementer og tegn dem igen når funktionen bliver kaldt
+function displayFinishedTask(showTask){
+    const clone = document.querySelector("template#toDoListe").content.cloneNode(true);
+    
+    clone.querySelector("[data-field=taskString]").textContent = showTask.name;
+    clone.querySelector("[data-field=taskAmount]").textContent = showTask.amount;
+    clone.querySelector("[data-id=defaultID]").setAttribute("data-id", showTask.id);
+    
+    document.querySelector("#finishedTasks").appendChild(clone);
+    
+    document.querySelectorAll("[data-type=button]").forEach(button=>{
+        //button.parentNode.parentNode.removeChild(button);
+        button.style.display = 'none';
+        //button.style.width = '0';
+        //button.style.height = '0';
+    })
+    
+    //btnEventListener();
+}
+/* document.querySelectorAll("#tasks div button").forEach(button=>{
+    button.addEventListener("click", btnFilterEVTListen)});
+    
+    function btnFilterEVTListen(evt){
+        console.log(evt.currentTarget.dataset.field);
+        
+    }
+    */
+   
+   
+   //
+   
+   //DONE ----- Tilføj item til liste //generer id til liste //check om ID'et er taget (generer måske ID'et baseret på hvor mange tasks du har lavet)
+   
+   //fjerne item fra listen //føj til "trash array" (array med max 5 objecter, objecter kan restoreres)
+   
+   //Tegn hvert item i arrayet (se i animal base filtering) //clear alle elementer og tegn dem igen når funktionen bliver kaldt
